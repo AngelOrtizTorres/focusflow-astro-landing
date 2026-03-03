70ea2f523c70838701a71de499293c33fe977228
Ángel Ortiz Torres <4ngel0rt0305@gmail.com>
2026-03-03 14:37:33 +0100

    feat: update Astro configuration and package dependencies for Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  integrations: [],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwind()],
+  },
 });
